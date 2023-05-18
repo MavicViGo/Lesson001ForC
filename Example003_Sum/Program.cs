0a415b92ba6d3d6e52b4d047b4101d692d7e0001
Goncharov <GVVic@mail.ru>
2023-05-18 20:04:09 +0700

    Add third Example

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите A");
+int numberA = Console.Read();
+Console.WriteLine("Введите B");
+int numberB = Console.Read();
+int result = numberA + numberB;
+Console.WriteLine(result);
